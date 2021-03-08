7316af48834308272f37225b1645b531bddc8ae4
Matheus P <matheus.pereira@gocase.com.br>
2021-03-08 15:34:51 -0300

    Translations portuguese

--- i18n/pt-BR/material_rules.js
+++ i18n/pt-BR/material_rules.js
@@ -1,18 +1,16 @@
 export default {
-  materialRules: {
-    title: 'Regras de Material',
-    new: 'Nova Regra',
-    form: 'Formulário de Regra',
-    submit: 'Submeter',
-    companyFilter: 'Filtro por Compania',
-    categoryFilter: 'Filtro por Categoria',
-    materialFilter: 'Filtro por Material',
-    forAll: 'Vale para a todas opções',
-    notAppliable: 'Não vale para nenhuma opção',
-    only: 'Vale somente para:',
-    except: 'Vale para todos exceto:',
-    id: 'Numero da regra',
-    createdAt: 'Created At',
-    actions: 'Actions'
-  }
+  title: 'Regras de Material',
+  new: 'Nova Regra',
+  form: 'Formulário de Regra',
+  submit: 'Submeter',
+  companyFilter: 'Filtro por Compania',
+  categoryFilter: 'Filtro por Categoria',
+  materialFilter: 'Filtro por Material',
+  forAll: 'Vale para a todas opções',
+  notAppliable: 'Não vale para nenhuma opção',
+  only: 'Vale somente para:',
+  except: 'Vale para todos exceto:',
+  id: 'Numero da regra',
+  createdAt: 'Created At',
+  actions: 'Actions'
 }
